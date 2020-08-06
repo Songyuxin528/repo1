42a1c402c4068a1f7d737fef929f62e188b2f5f4
songyuxin <1178306549@qq.com>
2020-08-06 10:26:21 +0800

    我改变了一部分文件

--- com/src/firstJava/myFirstJava.java
+++ com/src/firstJava/myFirstJava.java
@@ -4,5 +4,9 @@ public class myFirstJava {
     public static void main(String[] args) {
         System.out.println("你好啊，任婧。");
         System.out.println("我很好啊");
+
+        for (int i = 0 ; i < 10 ; i++) {
+            System.out.println(i);
+        }
     }
 }
